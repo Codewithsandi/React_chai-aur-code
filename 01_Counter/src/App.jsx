4025061b7a699bbc164a7a1e4143c155015a124f
Sandipan Kundu <73771749+Codewithsandi@users.noreply.github.com>
2024-05-10 09:53:22 +0530

    interview question

--- 01_Counter/src/App.jsx
+++ 01_Counter/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import "./style.css";
 
 function App() {
-  const [counter, setCounter] = useState(16)
+  const [counter, setCounter] = useState(10)
   const [disabledBTN, setDisabledBTN] = useState(false)
 
   const addNumber = () => {
@@ -11,6 +11,22 @@ function App() {
     }
     if (counter < 20) {
       setCounter(counter + 1)
+
+      //! for interview purpose 
+      //? if we use 
+      //? setCounter(counter + 1)
+      //? setCounter(counter + 1)
+      //? setCounter(counter + 1)
+
+      //* it cant increment all of this because 
+      //* all of this setCounter update a same counter value so useState generate a batch and check.
+      //* here we only upadte same counter value so it cant execute all setCounter function. 
+
+      //* if we want to increase all of thease then do this
+      //? setCounter((prevCounter) => prevCounter + 1)
+      //? setCounter((prevCounter) => prevCounter + 1)
+      //? setCounter((prevCounter) => prevCounter + 1)
+      
       setDisabledBTN(false)
     }
   }
